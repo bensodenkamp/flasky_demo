4dcec61926f47bee4bc21619d8fc2c7abdfe10f6
Benjamin Sodenkamp <54522068+bensodenkamp@users.noreply.github.com>
2023-05-03 20:32:50 -0600

    added docstring

--- app/demo.py
+++ app/demo.py
@@ -1,8 +1,9 @@
+"""Module providingFunction Main App Entrypoint"""
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def hello_world():
+    """Handles the / route"""
     return "<p>Hello, World!</p>"
