542d5d1d161a2ae6e511c3f3b216c3704181daa5
Benjamin Sodenkamp <54522068+bensodenkamp@users.noreply.github.com>
2023-05-03 20:14:11 -0600

    Ran 'black' formatter

--- app/tests_main.py
+++ app/tests_main.py
@@ -1,19 +1,20 @@
 import unittest
 from demo import app
 
+
 class Tests(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
     def test_app_endpoint(self):
-
         response = self.app.get("/")
         # print (response.data)
         self.assertEqual(
             response.data,
-            b'<p>Hello, World!</p>',
+            b"<p>Hello, World!</p>",
         )
         self.assertEqual(response.status_code, 200)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
